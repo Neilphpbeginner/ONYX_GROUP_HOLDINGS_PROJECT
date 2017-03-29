f3a02c8fda5bf77a4c49f8aa7b6c773005d2152d
Neilphpbeginner <lemmer.neil@gmail.com>
2017-03-29 19:51:15 +0200

    Vesion

--- appvars.php
+++ appvars.php
@@ -1,8 +1,10 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_USER_PASSWORD','');
-define('DB_HOST_DATA','onyx_group');
+// Defining the app variables to connect to the MYSQL Databae.
+
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_USER_PASSWORD','');
+    define('DB_HOST_DATA','onyx_group');
 
 ?>
